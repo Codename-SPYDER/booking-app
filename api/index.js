eaae79822df10cd8f36b2830938a3752fd7d97ce
Siddiq <siddiq.adam.khan49@outlook.com>
2023-03-29 12:17:35 -0400

    cors change

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cors({
 	credentials: true,
-	origin: 'http://localhost:5173'
+	origin: process.env.HOST_URL
 }));
 
 async function uploadToS3(path, originalFilename, mimetype) {
