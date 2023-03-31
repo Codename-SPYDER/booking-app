fdc7c766084b3b5710055f7bbda6c393fe932e74
Siddiq Khan <104534833+SID-dev-droid@users.noreply.github.com>
2023-03-31 18:29:13 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -31,6 +31,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: 'https://booking-app-req.vercel.app',
+}));
 
 async function uploadToS3(path, originalFilename, mimetype) {
 	const client = new S3Client({
