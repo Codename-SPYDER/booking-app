d0d5e84f1a559a4b0dc017484baab5ed9942ca38
Siddiq <siddiq.adam.khan49@outlook.com>
2023-05-01 15:19:07 -0400

    overflow  change

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -13,30 +13,30 @@ export default function Header() {
 	return(
 		
 	<header className='flex justify-between'>
-		{help ? (
-  <div className="overflow-y-auto fixed w-full h-screen bg-primary/60 flex flex-col justify-center items-center z-20 top-0 left-0">
-		<p className="bg-gray-100 mb-24 w-3/4 2xl:w-1/2 p-8 rounded-md xl:text-lg border-gray-400 border-2">
-			Hi, welcome to my airbnb clone Webapp! <br/><br/> Allow me to provide you with a short tutorial.
-			You can browse and view property listings through the main page; however in order to book a stay you must first create an account. 
-			You can login or create an account by clicking the profile icon in the top right where you will be directed to the login/register page.
-			After logging in and then booking a stay you can view your booking in the My bookings section of your user account, which you can access by clicking your user icon again. 
-			<br/><br/>You also have the option of adding properties that you will be hosting. To add a new property listing, click on the user icon, select My Accommodations, and then click on the 'Add New Place' button.
-			When creating a new listing, you have the option of selecting a picture which
-			users can view when first visiting the site. If you have to make changes regarding your properties information,
-			simply click on the listing from your My Accommodations page, change the information, and then resubmit it<br/><br/> 
-			Please feel free to communicate any issues you face when using the application or improvements you would like to see through the Contact Me portion of my profile website.
-			Thanks again for stopping by!
-			
-			</p>
-    <button onClick={toggleHelp} className="text-lg w-1/2 text-white py-2 rounded-full bg-primary border-white shadow-md hover:scale-95 ease-in-out duration-500">Back to Site</button>
-  </div>
-) : null}
+		{help ? 
+		(
+  		<div className=" fixed w-full h-screen bg-primary/60 flex flex-col justify-center items-center z-20 top-0 left-0">
+				<p className="overflow-y-auto bg-gray-100 mb-24 w-3/4 2xl:w-1/2 p-8 rounded-md xl:text-lg border-gray-400 border-2 max-[600px]:text-sm max-[600px]:p-3 max-[600px]:w-full max-[600px]:mb-10">
+					Hi, welcome to my airbnb clone Webapp! <br/><br/> Allow me to provide you with a short tutorial.
+					You can browse and view property listings through the main page; however in order to book a stay you must first create an account. 
+					You can login or create an account by clicking the profile icon in the top right where you will be directed to the login/register page.
+					After logging in and then booking a stay you can view your booking in the My bookings section of your user account, which you can access by clicking your user icon again. 
+					<br/><br/>You also have the option of adding properties that you will be hosting. To add a new property listing, click on the user icon, select My Accommodations, and then click on the 'Add New Place' button.
+					When creating a new listing, you have the option of selecting a picture which
+					users can view when first visiting the site. If you have to make changes regarding your properties information,
+					simply click on the listing from your My Accommodations page, change the information, and then resubmit it<br/><br/> 
+					Please feel free to communicate any issues you face when using the application or improvements you would like to see through the Contact Me portion of my profile website.
+					Thanks again for stopping by!
+				</p>
+    		<button onClick={toggleHelp} className="text-lg w-1/2 text-white py-2 rounded-full bg-primary border-white shadow-md hover:scale-95 ease-in-out duration-500">Back to Site</button>
+  		</div>
+		) : null}
 		<Link to={'/'} className='flex items-center gap-1'>
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-10 h-10">
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-10 h-10 hidden sm:flex">
 				<path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
 				<path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
 			</svg>
-			<span className='font-bold text-xl hidden sm:flex'>airbnb</span>
+			<span className='font-bold text-xl'>airbnb</span>
 		</Link>
 		<div className='hidden border border-gray-300 rounded-full py-2 px-4 gap-2 ml-12 shadow-md shadow-gray-300 md:flex'>
 			<div>Anywhere</div>
